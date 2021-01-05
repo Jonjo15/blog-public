73ff71ec4252dcdb01b85c9a9f536b6e96dcaaa0
IVAN <ivanbatur5g@gmail.com>
2021-01-05 21:53:22 +0100

    Link to individual blogpost works

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,9 +1,11 @@
 import React from 'react'
-// import {Link} from "react-router-dom"
+import {Link} from "react-router-dom"
 export default function PostCard({post}) {
+    let linkString = "/posts/" + post._id
     return (
+        
         <div>
-            <h2>{post.title} by {post.author.first_name} {post.author.family_name}</h2>
+            <Link to={linkString}><h2>{post.title} by {post.author.first_name} {post.author.family_name}</h2></Link>
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import axios from "axios"
 import PostCard from "../components/PostCard"
 export default function Home() {
+    
   const [posts, setPosts] = useState([])
   const [loading, setLoading] = useState(true)
 
